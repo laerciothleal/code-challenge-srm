f93649db584f98ea0df162b07c729d07bed7c926
Laercio  Leal <laercioleal@Laercios-MacBook-Pro.local>
2024-12-11 16:59:28 -0300

    updating

--- src/main/java/com/backend/controller/request/ItemComercializadoRequest.java
+++ src/main/java/com/backend/controller/request/ItemComercializadoRequest.java
@@ -1,9 +1,6 @@
 package com.backend.controller.request;
 
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 
 import java.math.BigDecimal;
@@ -11,9 +8,11 @@ import java.math.BigDecimal;
 @Builder
 public record ItemComercializadoRequest(
         @NotBlank(message = "O nome do item não pode estar nulo/branco")
+        @Max(value = 50, message = "O nome do item não pode ter mais que 50 caracteres")
         String nomeItem,
 
         @NotBlank(message = "O reino de origem não pode estar nulo/branco")
+        @Max(value = 50, message = "O reino não pode ter mais que 50 caracteres")
         String reinoOrigem,
 
         @NotNull(message = "O preço base não pode estar nulo")
--- src/main/java/com/backend/controller/request/MoedaRequest.java
+++ src/main/java/com/backend/controller/request/MoedaRequest.java
@@ -1,9 +1,6 @@
 package com.backend.controller.request;
 
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 
 import java.math.BigDecimal;
@@ -11,6 +8,7 @@ import java.math.BigDecimal;
 @Builder
 public record MoedaRequest(
         @NotBlank(message = "O nome da moeda não pode estar nulo/branco")
+        @Max(value = 50, message = "O nome da moeda não pode ter mais que 50 caracteres")
         String nomeMoeda,
 
         @NotNull(message = "A taxa de câmbio não pode estar nula")
